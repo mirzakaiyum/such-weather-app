95b3a1b5fb454e85cef351bf094ec58059d60a3d
mirzakaiyum <makfissh@gmail.com>
2023-11-18 19:20:28 +0600

    Added handling location not found

--- src/App.jsx
+++ src/App.jsx
@@ -15,57 +15,73 @@ function App() {
   const [searchTerm, setSearchTerm] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
-  
+
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const inputRef = useRef(null);
-
 
   function handleSearch(event) {
     event.preventDefault();
     setSearchTerm(inputRef.current.value);
   }
 
-  useEffect(function (){
-    async function fetchWeather() {
-      setLoading(true);
-      setError("");
-  
-      try {
-        if (!searchTerm) {
-          return;
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      const signal = controller.signal;
+      
+      async function fetchWeather() {
+        try {
+          setLoading(true);
+              setError("");
+
+          if (!searchTerm) {
+            return;
+          }
+
+          const locationURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=${WEATHER_API}`;
+          const locationRes = await fetch(locationURL, { signal });
+
+          if (!locationRes.ok) {
+            throw new Error(`Error: ${locationRes.status}`);
+          }
+
+          const locationData = await locationRes.json();
+
+          if (locationData.length === 0) {
+            throw new Error("Location not found");
+          }
+
+          const query = { lat: locationData[0].lat, lon: locationData[0].lon };
+
+          const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${query.lat}&lon=${query.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${WEATHER_API}`;
+          const weatherRes = await fetch(weatherURL, { signal });
+
+          if (!weatherRes.ok) {
+            throw new Error(`Error: ${weatherRes.status}`);
+          }
+
+          const weatherData = await weatherRes.json();
+          setForecast(
+            weatherData.daily?.filter((_, index) => index > 0 && index < 6)
+          );
+          setCurrentWeather(weatherData.daily);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
         }
-  
-        const locationURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=${WEATHER_API}`;
-        const locationRes = await fetch(locationURL);
-  
-        if (!locationRes.ok) {
-          throw new Error(`Error: ${locationRes.status}`);
-        }
-  
-        const locationData = await locationRes.json();
-        const query = { lat: locationData[0].lat, lon: locationData[0].lon };
-  
-        const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${query.lat}&lon=${query.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${WEATHER_API}`;
-        const weatherRes = await fetch(weatherURL);
-  
-        if (!weatherRes.ok) {
-          throw new Error(`Error: ${weatherRes.status}`);
-        }
-  
-        const weatherData = await weatherRes.json();
-        setForecast(weatherData.daily?.filter((_, index) => index > 0 && index < 6));
-        setCurrentWeather(weatherData.daily);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
       }
-    }
-  
-    fetchWeather();
-  }, [searchTerm]);
+
+      fetchWeather();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [searchTerm]
+  );
 
   return (
     <>
@@ -73,7 +89,8 @@ function App() {
       <TodayWeather currentWeather={currentWeather} />
       <Search handleSearch={handleSearch} inputRef={inputRef} />
       <DetailedWeather forecast={forecast}>
-        {loading && <p >Loading...</p>}
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
         {!loading && !error && (
           <>
             <TodayDetails currentWeather={currentWeather} />
